43762e8e6477d0106b92da53e1c6785da43c7ec3
gbouras13 <george.bouras@adelaide.edu.au>
2023-10-15 17:12:04 +1030

    add phold remote

--- src/phold/__init__.py
+++ src/phold/__init__.py
@@ -27,6 +27,8 @@ from phold.utils.validation import instantiate_dirs
 
 from phold.features.run_foldseek import run_foldseek_search, create_result_tsv
 
+from phold.features.query_remote_3Di import query_remote_3di
+
 # from phold.utils.validation import (
 #     check_evalue,
 #     instantiate_dirs,
@@ -117,7 +119,7 @@ def main_cli():
 
 
 """
-Chromosome command
+run command
 """
 
 
@@ -210,6 +212,153 @@ def run(
     fasta_3di: Path = Path(output) / "output3di.fasta"
     get_embeddings( cds_dict, output, model,  half_precision=True,    
                    max_residues=3000, max_seq_len=1000, max_batch=100 ) 
+    
+    ############
+    # create foldseek db
+    ############
+
+    foldseek_query_db_path: Path = Path(output) / "foldseek_db"
+    foldseek_query_db_path.mkdir(parents=True, exist_ok=True)
+
+    generate_foldseek_db_from_aa_3di(fasta_aa, fasta_3di, foldseek_query_db_path, logdir, prefix )
+
+    ###########
+    # run foldseek search
+    ###########
+
+    short_db_name = f"{prefix}_foldseek_database"
+    query_db: Path = Path(foldseek_query_db_path) / short_db_name
+    target_db: Path = Path(database) / "toy_prophage_db"
+
+    # make result and temp dirs 
+    result_db_base: Path = Path(output) / "result_db"
+    result_db_base.mkdir(parents=True, exist_ok=True)
+    result_db: Path = Path(result_db_base) / "result_db"
+
+    temp_db: Path = Path(output) / "temp_db"
+    temp_db.mkdir(parents=True, exist_ok=True)
+
+    # run foldseek search
+    run_foldseek_search(query_db, target_db,result_db, temp_db, threads, logdir )
+
+    # make result tsv 
+    result_tsv: Path =  Path(output) / "foldseek_results.tsv"
+    create_result_tsv(query_db, target_db, result_db, result_tsv, logdir)
+
+    # calculate tophits 
+    top_hits_tsv: Path = Path(output) / "tophits.tsv"
+
+    filtered_tophits_df = get_tophits(result_tsv, top_hits_tsv, evalue)
+
+
+
+
+    # validates fasta
+    #validate_fasta(input)
+
+    # validate e value
+    #check_evalue(evalue)
+
+
+
+    # end phold
+    end_phold(start_time)
+
+
+"""
+remote command
+"""
+
+
+@main_cli.command()
+@click.help_option("--help", "-h")
+@click.version_option(get_version(), "--version", "-V")
+@click.pass_context
+@common_options
+@click.option(
+    "-e",
+    "--evalue",
+    default="1e-3",
+    help="e value threshold for Foldseek",
+    show_default=True,
+)
+def remote(
+    ctx,
+    input,
+    output,
+    threads,
+    prefix,
+    evalue,
+    force,
+    model,
+    database,
+    **kwargs,
+):
+    """Runs phold"""
+
+    # validates the directory  (need to before I start phold or else no log file is written)
+    instantiate_dirs(output, force)
+
+    output: Path = Path(output)
+    logdir: Path = Path(output) / "logs"
+
+    params = {
+        "--input": input,
+        "--output": output,
+        "--threads": threads,
+        "--force": force,
+        "--prefix": prefix,
+        "--evalue": evalue
+    }
+
+
+    # initial logging etc
+    start_time = begin_phold(params)
+
+    # validates fasta
+    gb_dict = get_genbank(input)
+    if not gb_dict:
+        logger.warning("Error: no sequences found in genbank file")
+        logger.error("No sequences found in genbank file. Nothing to annotate")
+
+    # for key, value in gb_dict.items():
+    #     logger.info(f"Parameter: {key} {value}.")
+
+    # Create a nested dictionary to store CDS features by contig ID
+    cds_dict = {}
+
+    fasta_aa: Path = Path(output) / "outputaa.fasta"
+
+    # makes the nested dictionary {contig_id:{cds_id: cds_feature}}
+    
+    for record_id, record in gb_dict.items():
+        
+        cds_dict[record_id] = {}
+
+        for cds_feature in record.features:
+            if cds_feature.type == 'CDS':
+                cds_dict[record_id][cds_feature.qualifiers['ID'][0]] = cds_feature
+
+    ## write the CDS to file
+
+    with open(fasta_aa, 'w+') as out_f:
+        for contig_id, rest in cds_dict.items():
+
+            aa_contig_dict = cds_dict[contig_id]
+
+            # writes the CDS to file
+            for seq_id, cds_feature in aa_contig_dict.items():
+                out_f.write(f">{contig_id}:{seq_id}\n")
+                out_f.write(f"{cds_feature.qualifiers['translation'][0]}\n")
+
+    ############
+    # prostt5
+    ############
+
+    # generates the embeddings using ProstT5 and saves them to file
+    fasta_3di: Path = Path(output) / "output3di.fasta"
+    
+    query_remote_3di(cds_dict, output)
     
     ############
     # create foldseek db
