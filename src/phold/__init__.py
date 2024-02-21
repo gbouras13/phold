af856dc8077603739047cac07191e5bb8b9a4097
gbouras13 <george.bouras@adelaide.edu.au>
2024-02-21 15:00:45 +1030

    phold run

--- src/phold/__init__.py
+++ src/phold/__init__.py
@@ -311,6 +311,7 @@ def run(
         split_threshold=split_threshold,
         remote_flag=True,
         proteins_flag=False,
+        fasta_flag=fasta_flag,
         separate=separate,
         max_seqs=max_seqs
     )
