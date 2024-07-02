d43fba0c79ce674f9849406a1cf60cf402c54ecc
Eric  DEVEAUD <edeveaud@pasteur.fr>
2024-07-02 12:44:48 +0200

    set db dir under PHOLD_DBDIR environement variable control

--- src/phold/utils/constants.py
+++ src/phold/utils/constants.py
@@ -1,9 +1,9 @@
 from pathlib import Path
-
+import os
 repo_root = Path(__file__).parent.parent.resolve()
 
 CNN_DIR = repo_root / "cnn/"
 
-DB_DIR = repo_root / "database/"
+DB_DIR = os.environ.get("PHOLD_DBDIR", repo_root / "database/")
 
 FINETUNE_DIR = repo_root / "finetune/"
